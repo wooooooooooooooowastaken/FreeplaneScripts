8d7f100fdff41423c9e0adaa677b45809562824e
Alexandre Viau <alexandreviau@gmail.com>
2018-01-02 11:18:15 +0200

    Added try-catch because it some cases the script fails to retrieved the title and if this script runs on multiple selected nodes, it was failing so no nodes had their titles set. So with try-catch only the nodes that fail are not set, the others will be set.

--- SetTextToLinkWebPageTitle.groovy
+++ SetTextToLinkWebPageTitle.groovy
@@ -1,10 +1,15 @@
 // @ExecutionModes({ON_SELECTED_NODE, ON_SELECTED_NODE_RECURSIVELY})
 
-// Set the text of the node to the webpage title of it's link
+// Version history:
+    // 2018-01-02_11.16.52: Added try-catch because it some cases the script fails to retrieved the title and if this script runs on multiple selected nodes, it was failing so no nodes had their titles set. So with try-catch only the nodes that fail are not set, the others will be set.
+
+// Set the text of the node to the webpage title of it's link.
 
 if (link =~ /http/) { // Is a url
-    htmlText = link.text.toURL().text
-    def title = (htmlText =~ /(?<=title>).*?(?=<)/)[0]
-    node.text = title
+    try {
+        htmlText = link.text.toURL().text
+        def title = (htmlText =~ /(?<=title>).*?(?=<)/)[0]
+        node.text = title
+    }
+    catch (any) {}
 }
-	
